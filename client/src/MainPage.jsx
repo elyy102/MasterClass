4af1df4a19079cad0524314dd5ae515431417682
elyy102 <103173379+elyy102@users.noreply.github.com>
2024-03-13 23:56:14 +0500

    new

---
+++ client/src/MainPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { logOut } from './redux/authSlice'
+import { useSelector } from 'react-redux'
+import { Profile_user } from './Profile_user'
+import Profile_admin from './Profile_admin'
+
+const MainPage = () => {
+
+    const token = useSelector((state) => state.auth.token)
+    const role = useSelector((state) => state.auth.role)
+
+    return (
+        <>
+            {
+                role === "ADMIN" ? 
+                <Profile_admin /> 
+                : 
+                <Profile_user />
+            }
+        </>
+    )
+}
+
+export default MainPage
---
+++ client/src/ModalRecord.jsx
@@ -0,0 +1,25 @@
+const ModalRecord = ({ show, onCloseButtonClick }) => {
+    if (!show) {
+      return null;
+    }
+  
+    return (
+      <div className="modal-wrapper">
+        <div className="modal">
+          <div className="modal_head">
+            <p className="p_modal_head">Запись</p>
+            <button className="close_card" onClick={onCloseButtonClick}>х</button>
+          </div>
+          <div className="modal_content">
+            <input className='login_input' placeholder='Введите ФИО...' type="text" />
+            <input  className='password_input' placeholder='Введите номер телефона...' type="password" />
+            <input className='login_input' placeholder='Введите email...' type="email" />
+            <button className='logout_btn'>Записаться</button>
+        </div>
+        </div>
+      </div>
+    );
+};
+  
+export default ModalRecord;
+  
